3b5b4a17875e30ecb66cd3c480bce8915f72c870
suraj yadav <surajy9898@gmail.com>
2023-11-21 09:56:56 +0530

    changed dirname for es6

--- index.js
+++ index.js
@@ -10,8 +10,11 @@ import premiumFeatureRouter from './routes/premiumFeature.js'
 import purchase from './routes/purchase.js'
 import cors from 'cors';
 import forgetpassword from './models/Forgotpassword.js';
-import path from 'path';
+import path from 'path';  // Move the import statement here
+import { fileURLToPath } from 'url';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
@@ -24,8 +27,8 @@ app.use("/api/v1/expense", expenseRouter);
 app.use('/api/v1/premium', premiumFeatureRouter);
 app.use('/api/v1/purchase', purchase);
 app.use('/password', resetpassword);
- 
-const port = process.env.PORT || 4000; 
+
+const port = process.env.PORT || 4000;
 
 app.get("/", (req, res) => {
     res.send("Nice working");
@@ -33,26 +36,25 @@ app.get("/", (req, res) => {
 });
 
 app.use((req, res) => {
-  res.sendFile(path.join(__dirname, `public/${req.url}`));
-})
+    res.sendFile(path.join(__dirname, `public/${req.url}`));
+});
 
 User.hasOne(forgetpassword);
-
 
 User.hasMany(Expense);
 Expense.belongsTo(User);
 
-User.hasMany(Order); 
+User.hasMany(Order);
 Order.belongsTo(User);
- 
-sequelize.sync({ force: false})
-  .then(() => {
-    console.log('Database synced');
-  })
-  .catch((err) => {
-    console.error('Database sync error:', err);
-  });
+
+sequelize.sync({ force: false })
+    .then(() => {
+        console.log('Database synced');
+    })
+    .catch((err) => {
+        console.error('Database sync error:', err);
+    });
 
 app.listen(port, () => {
-  console.log(`server is running fine on ${port}`);
+    console.log(`server is running fine on ${port}`);
 });
